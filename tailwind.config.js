e79566c37ccfe2fdd498540d015a4f8be9ea0d34
wahab <aderintoadetunji@gmail.com>
2023-05-19 08:34:19 +0100

    added valtio for state and route config

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires, no-undef
+const colors = require("tailwindcss/colors");
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        orange: colors.orange,
+        purple: colors.violet,
+      },
+    },
+  },
+  plugins: [],
+};
